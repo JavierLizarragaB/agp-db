f476965059ddb29cfcb68a49ad2e2a776bcac98c
pescoboza <psebastian01@hotmail.com>
2021-01-13 02:08:53 -0700

    :green_heart: CI scripts work!

---
+++ client/recreateBuildDir.js
@@ -0,0 +1,6 @@
+// Used to make sure the server/app/static/react/ dir exists
+// at cloudbuild runtime because empty dirs are ignored by GitHub.
+const fs = require('fs');
+const path = require('path');
+const dir = path.resolve(__dirname, '../server/app/static/react');
+!fs.existsSync(dir) && fs.mkdirSync(dir);
