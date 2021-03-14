5cae2dd2bec6949aabccbd961e258390a59fafe8
Javier Lizarraga <javierlizarraga.b@hotmail.com>
2021-03-14 14:37:32 -0700

    :construction: Adding jwt for authorization

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -22,13 +22,13 @@ def create_app(config):
     app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 
     db.init_app(app)
-    # login.init_app(app)
-    # bootstrap.init_app(app)
+    login.init_app(app)
+    bootstrap.init_app(app)
 
     # Add flask-admin views
-    admin.init_app(app)
-    from .models import User, Patients
-    admin.add_view(ModelView(User, "Usuarios"))
+    #admin.init_app(app)
+    #from .models import User, Patients
+    #admin.add_view(ModelView(User, "Usuarios"))
     # admin.add_view(ModelView(Patients, "Pacientes"))
 
     from .main import main as main_bp
@@ -37,8 +37,8 @@ def create_app(config):
     from .api import api as api_bp
     app.register_blueprint(api_bp, url_prefix="/api")
 
-    # from .auth import auth as auth_bp
-    # app.register_blueprint(auth_bp)
+    from .auth import auth as auth_bp
+    app.register_blueprint(auth_bp)
 
     if os.getenv("FLASK_ENV") == "development":
         print(app.url_map)
--- server/app/auth/views.py
+++ server/app/auth/views.py
@@ -1,12 +1,40 @@
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, jsonify, request,  current_app
 from flask_login import login_user
+import datetime
+import jwt
+from functools import wraps
+
 
 from . import auth
 from .forms import LoginForm
 from ..models import User
 
 
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        ticket = request.args.get("token")
 
+        if not ticket:
+            flash("Credenciales no encontrada.")
+            return redirect(url_for("auth.login"))
+
+        print(ticket)
+        print(current_app.config['SECRET_KEY'])
+        try:
+            jwt.decode(ticket, current_app.config['SECRET_KEY'])
+        except:
+            flash("Credenciales inválidas.->")
+            return redirect(url_for("auth.login"))
+        
+        return f(*args, **kwargs)
+    return decorated
+
+@auth.route("/private")
+@token_required
+def private():
+    flash("yes yes route.")
+    return redirect(url_for("auth.private"))
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
@@ -17,12 +45,15 @@ def login():
         user = User.objects(username=form.username.data).first()
 
         if user is None:
-            flash("Credenciales inválidas.")
+            flash("Credenciales inválidas.<-")
             return redirect(url_for("auth.login"))
 
         login_user(user)
+        token = jwt.encode({'user' : user.username, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=280)}, current_app.config['SECRET_KEY'])
+        print(token)
+        print(current_app.config['SECRET_KEY'])
         flash("Ha iniciado sesión como {}.".format(user.username))
-        return redirect(url_for("main.index"))
+        return redirect(url_for("auth.login"))
         
 
     return render_template("auth/login.html", form=form)
