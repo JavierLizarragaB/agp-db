22a1c800335c50abdf3cbc00c606991c07195cee
GmoWilliams <guillermowilliams01@gmail.com>
2021-05-13 00:57:48 -0700

    Responsive and Updated Carousel

--- client/src/components/carrusel.component.js
+++ client/src/components/carrusel.component.js
@@ -14,48 +14,54 @@ import { Carousel } from 'react-bootstrap';
 
 export const Carrusel = () => {
     return (
+        <div className="my__carousel_main">
         <Carousel>
-            <Carousel.Item>
+            
+            <Carousel.Item interval={2000} >
                     <img
                     
-                    classname="img-carrusel"
+                    
                     
                     src={agp}
+                    width="900" height="400"
                     alt="First slide"
                     />
-                <Carousel.Caption>
-                    <h3>First slide label</h3>
+                <Carousel.Caption style={{color: "black", backgroundColor: "lightblue", padding: "1%"}}>
+                    <h3 classname="title-carrusel">First slide label</h3>
                     <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                 </Carousel.Caption>    
             </Carousel.Item>
                 
-            <Carousel.Item>
+            <Carousel.Item interval={2000} >
                     <img
-                    classname="img-carrusel"
+                    
                     
                     src={agp1}
+                    width="900" height="400"
                     alt="Second slide"
                     />
-                <Carousel.Caption>
-                    <h3>Second slide label</h3>
+                <Carousel.Caption style={{color: "black", backgroundColor: "lightblue", padding: "1%"}}>
+                    <h3 classname="title-carrusel">Second slide label</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </Carousel.Caption>
             </Carousel.Item>
                 
-            <Carousel.Item>
+            <Carousel.Item interval={2000} >
                     <img
-                    classname="img-carrusel"
+                    
                     
                     src={agp3}
+                    width="900" height="400"
                     alt="Third slide"
                     />
 
-                    <Carousel.Caption>
-                    <h3>Third slide label</h3>
+                    <Carousel.Caption style={{color: "black", backgroundColor: "lightblue", padding: "1%"}}>
+                    <h3 classname="title-carrusel">Third slide label</h3>
                     <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
                     </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
+        </div>
     );
 };
 
