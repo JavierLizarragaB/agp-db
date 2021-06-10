3570b78b2f03d675f83a3666ec5e7352d3ce28ec
Paulina Aldaco <paulina.aldaco.ro@gmail.com>
2021-06-10 13:05:29 -0700

    :construction: Working on form context

--- client/src/context/FormContext.js
+++ client/src/context/FormContext.js
@@ -54,9 +54,58 @@ class FormContextProvider extends Component {
         diseases_chronicdeg: ""
     }
 
+    diet = {
+        perceived_quality: "",
+        meals_per_day: "",
+        food_preparation: "",
+        water_per_day: "",
+        red_meat_week: null,
+        red_meat_month: null,
+        chicken_week: null,
+        chicken_month: null,
+        fish_week: null,
+        fish_month: null,
+        grain_week: null,
+        grain_month: null,
+        dairy_week:null,
+        dairy_month: null,
+        bread_week: null,
+        bread_month: null,
+        bread_pasta_week: null,
+        bread_pasta_month: null,
+        vegetables_fruits_week: null,
+        vegetables_fruits_month: null,
+    }
+
+    hygienePassPhysAct = {
+        shower_frequency: "",
+        toothbrushing_frequency: "",
+        home_hygiene: "",
+        phys_activity: "",
+        passtime: "",
+    }
+
+    others = {
+        how_found_out: "",
+        has_support_background: null,
+        notes_support_background: "",
+        observations: "",
+        social_plan: "",
+        socioeconomic_class: "",
+        social_worker: "",
+        animals: "",
+        vaccinated_animals: null,
+        ticks_animals: null,
+        diseases_animals: null,
+        vaccination_card: "",
+    }
+
     state = {
         general_info: this.generalInfo,
-        personal_background: this.personalBackground
+        personal_background: this.personalBackground,
+        diet: this.diet,
+        hygiene_pass_physact: this.hygienePassPhysAct,
+        others: this.others
     }
 
     updateGeneralInfo = (key,value) => {
@@ -79,11 +128,44 @@ class FormContextProvider extends Component {
         })
     }
 
+    updateDiet = (key,value) => {
+        this.setState({
+            ...this.state,
+            diet: {
+                ...this.state.diet,
+                [key]: value
+            }
+        })
+    }
+
+    updateHygienePassPhysAct = (key,value) => {
+        this.setState({
+            ...this.state,
+            hygiene_pass_physact: {
+                ...this.state.hygiene_pass_physact,
+                [key]: value
+            }
+        })
+    }
+
+    updateOthers = (key,value) => {
+        this.setState({
+            ...this.state,
+            others: {
+                ...this.state.others,
+                [key]: value
+            }
+        })
+    }
+
     render(){
         const contextValue = {
             formState:this.state,
             updateGeneralInfo:this.updateGeneralInfo,
             updatePersonalBackground:this.updatePersonalBackground,
+            updateDiet:this.updateDiet,
+            updateHygienePassPhysAct:this.updateHygienePassPhysAct,
+            updateOthers:this.updateOthers
         }
         return(
             <FormContext.Provider value={contextValue}>
