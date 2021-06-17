6e6bd690f8ba95c7304ffb57e6d1833d6a3e4b40
Prikense <a01251446@itesm.mx>
2021-06-17 16:33:56 -0700

    septima iteracion dropdown

--- client/src/components/septima-iteracion.component.js
+++ client/src/components/septima-iteracion.component.js
@@ -3,57 +3,95 @@ import { Button, Collapse } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class SeptimaIteracion extends Component {
+    constructor(){
+        super();
+        this.state ={
+            open1: false,
+	        open2: false,
+            open3: false
+        };
+    }
     render() {
         return (
             <>
-                <div className="form-title">
-                    Higiene
+                <div
+                    onClick={() => this.setState({open1: !(this.state.open1)})}
+                    aria-controls="form1"
+                    aria-expanded={this.state.open1}
+                    className="col-md-12 btn-text active btn btn-custom2 text-left">
+                    HIGIENE
                 </div>
-                <br />
+                <Collapse in={this.state.open1}>
+                    <div id="form1" className="formulario">
+                        <br />
 
-                <div className="form-row col-md-12">
-                    <div className="form-text col-md-3">¿Cuántas veces a la semana se baña?</div>
-                    <div className="col-md-1"/>
-                    <div className="form-group col-md-8">
-                        <textarea className="form-control form-pat" rows="1" />
-                    </div>
-                </div>
-                <br />
+                        <div className="form-row col-md-12">
+                            <div className="form-text col-md-3">¿Cuántas veces a la semana se baña?</div>
+                            <div className="col-md-1"/>
+                            <div className="form-group col-md-8">
+                                <textarea className="form-control form-pat" rows="1" />
+                            </div>
+                        </div>
+                        <br />
 
-                <div className="form-row col-md-12">
-                    <div className="form-text col-md-3">¿Cuántas veces se cepilla los dientes?</div>
-                    <div className="col-md-1"/>
-                    <div className="form-group col-md-8">
-                        <textarea className="form-control form-pat" rows="1" />
-                    </div>
-                </div>
-                <br />
+                        <div className="form-row col-md-12">
+                            <div className="form-text col-md-3">¿Cuántas veces se cepilla los dientes?</div>
+                            <div className="col-md-1"/>
+                            <div className="form-group col-md-8">
+                                <textarea className="form-control form-pat" rows="1" />
+                            </div>
+                        </div>
+                        <br />
 
-                <div className="form-row col-md-12">
-                    <div className="form-text col-md-3">Higiene del Hogar:</div>
-                    <div className="col-md-1"/>
-                    <div className="form-group col-md-8">
-                        <textarea className="form-control form-pat" rows="4" />
+                        <div className="form-row col-md-12">
+                            <div className="form-text col-md-3">Higiene del Hogar:</div>
+                            <div className="col-md-1"/>
+                            <div className="form-group col-md-8">
+                                <textarea className="form-control form-pat" rows="4" />
+                            </div>
+                        </div>
+                        <br />
                     </div>
-                </div>
-                <br />
+                </Collapse>
 
-                <div className="form-row col-md-12">
-                    <div className="form-title col-md-3">Actividad Física</div>
-                    <div className="col-md-1"/>
-                    <div className="form-group col-md-8">
-                        <textarea className="form-control form-pat" rows="6" />
-                    </div>
+                <div
+                    onClick={() => this.setState({open2: !(this.state.open2)})}
+                    aria-controls="form2"
+                    aria-expanded={this.state.open2}
+                    className="col-md-12 btn-text active btn btn-custom2 text-left">
+                    ACTIVIDAD FÍSICA
                 </div>
-                <br />
+                <Collapse in={this.state.open2}>
+                    <div id="form2" className="formulario">
+                        <div className="form-row col-md-12">
+                            <div className="form-text col-md-3">Actividad Física</div>
+                            <div className="col-md-1"/>
+                            <div className="form-group col-md-8">
+                                <textarea className="form-control form-pat" rows="6" />
+                            </div>
+                        </div>
+                        <br />
+                    </div>
+                </Collapse>
 
-                <div className="form-row col-md-12">
-                    <div className="form-title col-md-3">Pasatiempo</div>
-                    <div className="col-md-1"/>
-                    <div className="form-group col-md-8">
-                        <textarea className="form-control form-pat" rows="6" />
-                    </div>
+                <div
+                    onClick={() => this.setState({open3: !(this.state.open3)})}
+                    aria-controls="form3"
+                    aria-expanded={this.state.open3}
+                    className="col-md-12 btn-text active btn btn-custom2 text-left">
+                    PASATIEMPO
                 </div>
+                <Collapse in={this.state.open3}>
+                    <div id="form3" className="formulario">
+                        <div className="form-row col-md-12">
+                            <div className="form-text col-md-3">Pasatiempo</div>
+                            <div className="col-md-1"/>
+                            <div className="form-group col-md-8">
+                                <textarea className="form-control form-pat" rows="6" />
+                            </div>
+                        </div>
+                    </div>
+             </Collapse>
             </>
         )
     }
