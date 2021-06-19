b17c2847730285028dbbfe176e8c6dd220dee130
GmoWilliams <guillermowilliams01@gmail.com>
2021-06-18 18:03:41 -0700

    Added Calculadora de Egresos

--- client/src/components/quinta-iteracion.component.js
+++ client/src/components/quinta-iteracion.component.js
@@ -2,6 +2,8 @@ import React, { Component, useState } from "react";
 import { Button, Collapse } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+
 class QuintaIteracion extends Component {
     constructor(){
         super();
@@ -11,9 +13,174 @@ class QuintaIteracion extends Component {
             open3: false,
             open4: false,
             open5: false,
+            num1: "",
+            num2: "",
+            num3: "",
+            num4: "",
+            num5: "",
+            num6: "",
+            num7: "",
+            num8: "",
+            num9: "",
+            num10: "",
+            num11: "",
+            result: 0
         };
+        this._changeNum1 = this._changeNum1.bind(this);
+        this._changeNum2 = this._changeNum2.bind(this);
+        this._changeNum3 = this._changeNum3.bind(this);
+        this._changeNum4 = this._changeNum4.bind(this);
+        this._changeNum5 = this._changeNum5.bind(this);
+        this._changeNum6 = this._changeNum6.bind(this);
+        this._changeNum7 = this._changeNum7.bind(this);
+        this._changeNum8 = this._changeNum8.bind(this);
+        this._changeNum9 = this._changeNum9.bind(this);
+        this._changeNum10 = this._changeNum10.bind(this);
+        this._changeNum11 = this._changeNum11.bind(this);
     }
+
+        _changeNum1(e) {
+        if (e.target.validity.valid) {
+        var newNum1 = +(e.target.value)
+        this.setState({
+            num1: newNum1,
+            result: newNum1 + this.state.num2 + this.state.num3 + this.state.num4 + this.state.num5 + this.state.num6
+                            + this.state.num7 + this.state.num8 + this.state.num9 + this.state.num10 + this.state.num11
+            }); 
+        }
+    }
+    
+        _changeNum2(e) {
+        if (e.target.validity.valid) {
+        var newNum2 = +(e.target.value)
+        this.setState({
+            num2: newNum2,
+            result: newNum2 + this.state.num1 + this.state.num3 + this.state.num4 + this.state.num5 + this.state.num6
+                            + this.state.num7 + this.state.num8 + this.state.num9 + this.state.num10 + this.state.num11
+            }); 
+        }
+    }
+
+        _changeNum3(e) {
+        if (e.target.validity.valid) {
+        var newNum3 = +(e.target.value)
+        this.setState({
+            num3: newNum3,
+            result: newNum3 + this.state.num1 + this.state.num2 + this.state.num4 + this.state.num5 + this.state.num6
+                            + this.state.num7 + this.state.num8 + this.state.num9 + this.state.num10 + this.state.num11
+            }); 
+        }
+    }
+
+        _changeNum4(e) {
+        if (e.target.validity.valid) {
+        var newNum4 = +(e.target.value)
+        this.setState({
+            num4: newNum4,
+            result: newNum4 + this.state.num1 + this.state.num2 + this.state.num3 + this.state.num5 + this.state.num6
+                            + this.state.num7 + this.state.num8 + this.state.num9 + this.state.num10 + this.state.num11
+            }); 
+        }
+    }
+
+        _changeNum5(e) {
+        if (e.target.validity.valid) {
+        var newNum5 = +(e.target.value)
+        this.setState({
+            num5: newNum5,
+            result: newNum5 + this.state.num1 + this.state.num2 + this.state.num3 + this.state.num4 + this.state.num6
+                            + this.state.num7 + this.state.num8 + this.state.num9 + this.state.num10 + this.state.num11
+            }); 
+        }
+    }
+
+        _changeNum6(e) {
+        if (e.target.validity.valid) {
+        var newNum6 = +(e.target.value)
+        this.setState({
+            num6: newNum6,
+            result: newNum6 + this.state.num1 + this.state.num2 + this.state.num3 + this.state.num4 + this.state.num5
+                            + this.state.num7 + this.state.num8 + this.state.num9 + this.state.num10 + this.state.num11
+            }); 
+        }
+    }
+    
+        _changeNum7(e) {
+        if (e.target.validity.valid) {
+        var newNum7 = +(e.target.value)
+        this.setState({
+            num7: newNum7,
+            result: newNum7 + this.state.num1 + this.state.num2 + this.state.num3 + this.state.num4 + this.state.num5
+                            + this.state.num6 + this.state.num8 + this.state.num9 + this.state.num10 + this.state.num11
+            }); 
+        }
+    }
+
+        _changeNum8(e) {
+        if (e.target.validity.valid) {
+        var newNum8 = +(e.target.value)
+        this.setState({
+            num8: newNum8,
+            result: newNum8 + this.state.num1 + this.state.num2 + this.state.num3 + this.state.num4 + this.state.num5
+                            + this.state.num6 + this.state.num7 + this.state.num9 + this.state.num10 + this.state.num11
+            }); 
+        }
+    }
+
+        _changeNum9(e) {
+        if (e.target.validity.valid) {
+        var newNum9 = +(e.target.value)
+        this.setState({
+            num9: newNum9,
+            result: newNum9 + this.state.num1 + this.state.num2 + this.state.num3 + this.state.num4 + this.state.num5
+                            + this.state.num6 + this.state.num7 + this.state.num8 + this.state.num10 + this.state.num11
+            }); 
+        }
+    }
+
+        _changeNum10(e) {
+        if (e.target.validity.valid) {
+        var newNum10 = +(e.target.value)
+        this.setState({
+            num10: newNum10,
+            result: newNum10 + this.state.num1 + this.state.num2 + this.state.num3 + this.state.num4 + this.state.num5
+                            + this.state.num6 + this.state.num7 + this.state.num8 + this.state.num9 + this.state.num11
+            }); 
+        }
+    }
+
+        _changeNum11(e) {
+        if (e.target.validity.valid) {
+        var newNum11 = +(e.target.value)
+        this.setState({
+            num11: newNum11,
+            result: newNum11 + this.state.num1 + this.state.num2 + this.state.num3 + this.state.num4 + this.state.num5
+                            + this.state.num6 + this.state.num7 + this.state.num8 + this.state.num9 + this.state.num10
+            }); 
+        }
+    }
+
+    /*
+    handlenum1 = (event) => {
+        this.setState({
+            num1: event.target.value
+        })
+    }
+
+    handlenum2 = (event) => {
+        this.setState({
+            num2: event.target.value
+        })
+    }
+
+    handleTotal = (event) => {
+        this.setState({total: parseInt(this.state.num1) + parseInt(this.state.num2) });
+        event.prevent.default();
+    }
+    */
+
     render() {
+
         return (
             <>
                 <div
@@ -350,95 +517,99 @@ class QuintaIteracion extends Component {
                     aria-expanded={this.state.open5}
                     className="col-md-12 btn-text active btn btn-custom2 text-left">
                     EGRESOS
+
                 </div>
                 <Collapse in={this.state.open5}>
                     <div id="form5" className="formulario">
                         <br />
 
-                        <div className="form-row">
-                            <div className="col-md-1" />
-                            <div className="form-group col-md-3">Energía Eléctrica</div>
-                            <div className="form-group col-md-2">
-                                <input type="number" className="form-control form-pat" placeholder="$" />           
+                        <form >
+                            <div className="form-row">
+                                <div className="col-md-1" />
+                                <div className="form-group col-md-3">Energía Eléctrica</div>
+                                <div className="form-group col-md-2">
+                                    <input type="number" className="form-control form-pat" placeholder="$" value={this.state.num1} onChange={this._changeNum1} step="any" />           
+                                </div>
                             </div>
-                        </div>
-                        <div className="form-row">
-                            <div className="col-md-1" />
-                            <div className="form-group col-md-3">Agua</div>
-                            <div className="form-group col-md-2">
-                                <input type="number" className="form-control form-pat" placeholder="$" />           
+                            <div className="form-row">
+                                <div className="col-md-1" />
+                                <div className="form-group col-md-3">Agua</div>
+                                <div className="form-group col-md-2">
+                                    <input type="number" className="form-control form-pat" placeholder="$" value={this.state.num2} onChange={this._changeNum2} step="any" />           
+                                </div>
                             </div>
-                        </div>
-                        <div className="form-row">
-                            <div className="col-md-1" />
-                            <div className="form-group col-md-3">Gas</div>
-                            <div className="form-group col-md-2">
-                                <input type="number" className="form-control form-pat" placeholder="$" />           
+                            <div className="form-row">
+                                <div className="col-md-1" />
+                                <div className="form-group col-md-3">Gas</div>
+                                <div className="form-group col-md-2">
+                                    <input type="number" className="form-control form-pat" placeholder="$" value={this.state.num3} onChange={this._changeNum3} step="any" />           
+                                </div>
                             </div>
-                        </div>
-                        <div className="form-row">
-                            <div className="col-md-1" />
-                            <div className="form-group col-md-3">Teléfono</div>
-                            <div className="form-group col-md-2">
-                                <input type="number" className="form-control form-pat" placeholder="$" />           
+                            <div className="form-row">
+                                <div className="col-md-1" />
+                                <div className="form-group col-md-3">Teléfono</div>
+                                <div className="form-group col-md-2">
+                                    <input type="number" className="form-control form-pat" placeholder="$" value={this.state.num4} onChange={this._changeNum4} step="any" />           
+                                </div>
                             </div>
-                        </div>
-                        <div className="form-row">
-                            <div className="col-md-1" />
-                            <div className="form-group col-md-3">Alimentación</div>
-                            <div className="form-group col-md-2">
-                                <input type="number" className="form-control form-pat" placeholder="$" />           
+                            <div className="form-row">
+                                <div className="col-md-1" />
+                                <div className="form-group col-md-3">Alimentación</div>
+                                <div className="form-group col-md-2">
+                                    <input type="number" className="form-control form-pat" placeholder="$" value={this.state.num5} onChange={this._changeNum5} step="any" />           
+                                </div>
                             </div>
-                        </div>
-                        <div className="form-row">
-                            <div className="col-md-1" />
-                            <div className="form-group col-md-3">Renta</div>
-                            <div className="form-group col-md-2">
-                                <input type="number" className="form-control form-pat" placeholder="$" />           
+                            <div className="form-row">
+                                <div className="col-md-1" />
+                                <div className="form-group col-md-3">Renta</div>
+                                <div className="form-group col-md-2">
+                                    <input type="number" className="form-control form-pat" placeholder="$" value={this.state.num6} onChange={this._changeNum6} step="any" />           
+                                </div>
                             </div>
-                        </div>
-                        <div className="form-row">
-                            <div className="col-md-1" />
-                            <div className="form-group col-md-3">Transporte (o Gasolina)</div>
-                            <div className="form-group col-md-2">
-                                <input type="number" className="form-control form-pat" placeholder="$" />           
+                            <div className="form-row">
+                                <div className="col-md-1" />
+                                <div className="form-group col-md-3">Transporte (o Gasolina)</div>
+                                <div className="form-group col-md-2">
+                                    <input type="number" className="form-control form-pat" placeholder="$" value={this.state.num7} onChange={this._changeNum7} step="any" />           
+                                </div>
                             </div>
-                        </div>
-                        <div className="form-row">
-                            <div className="col-md-1" />
-                            <div className="form-group col-md-3">Educación</div>
-                            <div className="form-group col-md-2">
-                                <input type="number" className="form-control form-pat" placeholder="$" />           
+                            <div className="form-row">
+                                <div className="col-md-1" />
+                                <div className="form-group col-md-3">Educación</div>
+                                <div className="form-group col-md-2">
+                                    <input type="number" className="form-control form-pat" placeholder="$" value={this.state.num8} onChange={this._changeNum8} step="any" />           
+                                </div>
                             </div>
-                        </div>
-                        <div className="form-row">
-                            <div className="col-md-1" />
-                            <div className="form-group col-md-3">Vestido</div>
-                            <div className="form-group col-md-2">
-                                <input type="number" className="form-control form-pat" placeholder="$" />           
+                            <div className="form-row">
+                                <div className="col-md-1" />
+                                <div className="form-group col-md-3">Vestido</div>
+                                <div className="form-group col-md-2">
+                                    <input type="number" className="form-control form-pat" placeholder="$" value={this.state.num9} onChange={this._changeNum9} step="any" />           
+                                </div>
                             </div>
-                        </div>
-                        <div className="form-row">
-                            <div className="col-md-1" />
-                            <div className="form-group col-md-3">Diversión</div>
-                            <div className="form-group col-md-2">
-                                <input type="number" className="form-control form-pat" placeholder="$" />           
+                            <div className="form-row">
+                                <div className="col-md-1" />
+                                <div className="form-group col-md-3">Diversión</div>
+                                <div className="form-group col-md-2">
+                                    <input type="number" className="form-control form-pat" placeholder="$" value={this.state.num10} onChange={this._changeNum10} step="any" />           
+                                </div>
                             </div>
-                        </div>
-                        <div className="form-row">
-                            <div className="col-md-1" />
-                            <div className="form-group col-md-3">Otros</div>
-                            <div className="form-group col-md-2">
-                                <input type="number" className="form-control form-pat" placeholder="$" />           
+                            <div className="form-row">
+                                <div className="col-md-1" />
+                                <div className="form-group col-md-3">Otros</div>
+                                <div className="form-group col-md-2">
+                                    <input type="number" className="form-control form-pat" placeholder="$" value={this.state.num11} onChange={this._changeNum11} step="any" />           
+                                </div>
                             </div>
-                        </div>
-                        <div className="form-row">
-                            <div className="col-md-3" />
-                            <div className="form-group col-md-1 form-text">Total</div>
-                            <div className="form-group col-md-2">
-                                <input type="number" className="form-control form-pat" placeholder="$"/>           
+                            <div className="form-row">
+                                <div className="col-md-3" />
+                                <div className="form-group col-md-1 form-text">Total</div>
+                                <div className="form-group col-md-2">
+                                    <p className="form-control form-pat">$ {this.state.result}</p>
+                                </div>
                             </div>
-                        </div>
+                        </form>
+                        
                     </div>
                 </Collapse>
             </>
