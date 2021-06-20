92d1966a8c7382c07354b7d2083c9a78f4d5e66b
Javier Lizarraga <javierlizarraga.b@gmail.com>
2021-06-20 10:03:42 -0700

    🗑️ Cleaned code

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import Login from './components/login.component';
 import UserPanel from './components/userpanel.component';
-import DatosPaciente from './components/datospaciente.component';
 import Form from './components/form.component';
-import Inicio from './components/Inicio/index';
 import UserProvider, { Example as UserCtxExample } from './context/UserProvider';
 import Carrusel from './components/carrusel.component';
 import PagInicio from './components/Inicio/inicio';
@@ -30,7 +28,6 @@ function App() {
                             <ProtectedRoute path="/user-panel" component={UserPanel} isAuth={true}/>
                             <ProtectedRoute path="/datos-paciente" component={Form} isAuth={true}/>
                             <ProtectedRoute path="/directorio-paciente" component={DirectorioPaciente} isAuth={true}/>
-                            <ProtectedRoute path="/inicio" component={Inicio} isAuth={true}/>
                             <ProtectedRoute path="/carrusel" component={Carrusel} isAuth={true}/>
                             <ProtectedRoute path="/test/user-ctx" component={UserCtxExample} isAuth={true}/>
                             <ProtectedRoute path="/pag-inicio" component={PagInicio} isAuth={true}/>
