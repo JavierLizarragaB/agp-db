63b062b985b13ef841174a6c3fe466453c35d0c3
Javier Lizarraga <javierlizarraga.b@gmail.com>
2021-06-20 10:03:19 -0700

    🚧 Working on redirecting

--- client/src/components/navbar.component.js
+++ client/src/components/navbar.component.js
@@ -1,32 +1,34 @@
-import React, { Component, useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { useHistory } from 'react-router';
 import logo from '../img/agp-logo.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-class NavBar extends Component {
-    render() {
-        return (
-            <div>
-                <nav class="navbar navbar-expand-lg navbar-light">
-                    <a class="navbar-brand" href="#">
-                        <img src={logo} width="250" class="d-inline-block align-top" alt="" />
-                        <div id="menu-left">
-                            <Link className="nav-item" to={'../pag-inicio'}>
-                            Inicio
-                            </Link>{' '}
-                            <Link className="nav-item" to={'../directorio-paciente'}>
-                            Pacientes
-                            </Link>{' '}
-                            <Link className="nav-item" to={''}>
-                            Cerrar Sesión
-                            </Link>{' '}
-                        </div>
-                    <div className="collapse navbar-collapse" id="navbarTogglerDemo02"></div>
-                    </a>
-                </nav>
-            </div>
-        );
-    }
+export const NavBar = () => {
+
+    let history = useHistory();
+
+    return (
+        <div>
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <a class="navbar-brand" href="#">
+                    <img src={logo} width="250" class="d-inline-block align-top" alt="" />
+                    <div id="menu-left">
+                        <Link className="nav-item" onClick={()=>{history.push('/pag-inicio');}}>
+                        Inicio
+                        </Link>{' '}
+                        <Link className="nav-item" onClick={()=>{history.push('/directorio-paciente');}}>
+                        Pacientes
+                        </Link>{' '}
+                        <Link className="nav-item" onClick={()=>{history.push('/log-in');}}>
+                        Cerrar Sesión
+                        </Link>{' '}
+                    </div>
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo02"></div>
+                </a>
+            </nav>
+        </div>
+    );
 }
 
 export default NavBar;
